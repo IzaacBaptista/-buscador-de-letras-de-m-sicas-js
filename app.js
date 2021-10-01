41fb2a09851d9cf648f6e897ff944bb33e7a38cc
Izaac Baptista <izaacbaptista@gmail.com>
2021-10-01 15:16:34 -0300

    insertSongsIntoPage

--- app.js
+++ app.js
@@ -6,13 +6,34 @@ const songsContainer = document.querySelector('#songs-container')
 
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
+const apiURL = `https://api.lyrics.ovh`
+
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = songsInfo.data.map(song => `
+        <li class="song">
+            <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+        </li>
+    `).join('')
+}
+
+const fetchSongs = async term => {
+    const response = await fetch(`${apiURL}/suggest/${term}`)
+    const data = await response.json()
+    
+    insertSongsIntoPage(data)
+}
+
 form.addEventListener('submit', event => {
     event.preventDefault()
 
-    const searchTerm = searchInput.value
+    const searchTerm = searchInput.value.trim()
     
+    if(!searchTerm) {
+        songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo  válido!</li>`
+        return
+    }
     
+    fetchSongs(searchTerm)
 })
 
-const apiURL = `https://api.lyrics.ovh`
 
