895ff2dcc43f20be1b0c5b2600a2a63a961d0327
Izaac Baptista <izaacbaptista@gmail.com>
2021-10-02 18:01:01 -0300

    up

--- app.js
+++ app.js
@@ -8,12 +8,30 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
 
+const getMoreSongs = async url => {
+    const response = await fetch(url)
+    const data = await response.json()
+    
+    insertSongsIntoPage(data)
+}
+
 const insertSongsIntoPage = songsInfo => {
     songsContainer.innerHTML = songsInfo.data.map(song => `
         <li class="song">
             <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
         </li>
     `).join('')
+
+    if(songsInfo.prev || songsInfo.next) {
+        prevAndNextContainer.innerHTML = `
+            ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
+            ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
+        `
+        return
+    }
+
+    prevAndNextContainer.innerHTML
 }
 
 const fetchSongs = async term => {
